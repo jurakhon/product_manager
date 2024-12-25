06f4b3c42f7bca01f6e423717abfbc76f5fa1401
jurakhon <jurakhon@gmail.com>
2024-12-25 15:08:23 +0500

    adding menu structure with while true.

---
+++ products_manager.py
@@ -0,0 +1,15 @@
+
+
+
+
+
+while True:
+    print("Welcome to my products manager")
+    print("1. Add a product")
+    print("2. Update price")
+    print("3. Delete a product")
+    print("4. Total price")
+    print("5. Save products to a file")
+    print("6. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
