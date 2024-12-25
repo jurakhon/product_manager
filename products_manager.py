26d0c003b1e8876ce87af4958f2bfe6e736d922b
jurakhon <jurakhon@gmail.com>
2024-12-25 15:17:58 +0500

    adding products

--- products_manager.py
+++ products_manager.py
@@ -1,3 +1,10 @@
+products = {}
+
+def add_product():
+    name = input("Enter the name of the product: ")
+    price = int(input("Enter the price of the product: "))
+    products[name] = price
+    print(f"Product {name} added successfully")
 
 
 
@@ -10,6 +17,12 @@ while True:
     print("3. Delete a product")
     print("4. Total price")
     print("5. Save products to a file")
+    print("5. Print current list")
     print("6. Exit")
     choice = input("Enter your choice: ")
     if choice == "1":
+        add_product()
+
+    elif choice == "5":
+        print(products)
+
